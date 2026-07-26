bef0377ea7668cc65ee5d8d12c8e72b3c2ded24a
AanchalJha05 <aanchaljha207@gmail.com>
2026-07-26 14:58:47 +0530

    Update frontend API URL for production

--- frontend/src/services/authApi.js
+++ frontend/src/services/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authApi = axios.create({
-    baseURL: "http://127.0.0.1:8000/api",
+    baseURL: "https://cookpad-backend.onrender.com/api",
 });
 
 export const loginUser = (data) => {
