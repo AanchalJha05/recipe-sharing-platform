0f3803ca1a7e3d7d2fa4e6cff297355da79acd33
AanchalJha05 <aanchaljha207@gmail.com>
2026-07-27 21:30:33 +0530

    Configure Cloudinary storage

--- cookpad/settings.py
+++ cookpad/settings.py
@@ -162,3 +162,11 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.AllowAny",
     ),
 }
+import cloudinary
+
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True,
+)
